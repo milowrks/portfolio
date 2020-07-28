0c76fdfaf8e085fade614d80919efd8ea2c72c04
Milo Zheng <hello@milo.works>
2020-07-28 22:35:58 +1000

    feat: use fonts in global style

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,7 +1,17 @@
 <svelte:head>
-	<title>Contact</title>
+  <title>Contact</title>
 </svelte:head>
 
 <h1>Say Hi</h1>
 
-<p>Hello, I'm Milo.</p>
+<p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sollicitudin
+  ullamcorper leo et convallis. Mauris nec pretium nunc.
+</p>
+<p>
+  Nulla quis nisl quis diam dapibus dictum. Etiam euismod sagittis enim, eu
+  vehicula justo. Vestibulum aliquet et augue at tristique. Integer id neque sit
+  amet diam rhoncus ullamcorper. Praesent vestibulum maximus quam, sit amet
+  gravida dolor euismod at. Aliquam pharetra porta tortor, in sodales ante
+  hendrerit pharetra.
+</p>
