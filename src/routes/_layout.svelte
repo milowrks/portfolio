0b8531e8f9ec95452b2a95ad00e14803b92d12bc
Milo Zheng <hello@milo.works>
2020-08-30 16:31:02 +1000

    feat: add responsive layout grid
    Using custom properties
    Based on CSS-Tricks article;
    https://css-tricks.com/responsive-designs-and-css-custom-properties-building-a-flexible-grid-system/

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,40 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+  import Nav from "../components/Nav.svelte";
 
-	export let segment;
+  export let segment;
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  .header {
+    --width-sm: 12;
+  }
+
+  .content {
+    --width-sm: 12;
+    --width-md: 8; /* Tablet and larger */
+  }
+
+  .sidebar {
+    --width-sm: 12;
+    --width-md: 4; /* Tablet and larger */
+  }
+
+  main {
+    /* position: relative; */
+    /* max-width: 56em; */
+    /* background-color: white; */
+    /* padding: 2em; */
+    /* margin: 0 auto; */
+    box-sizing: border-box;
+  }
 </style>
 
-<Nav {segment}/>
-
-<main>
-	<slot></slot>
-</main>
+<div class="row">
+  <header class="header col">
+    <Nav {segment} />
+  </header>
+  <main class="content col">
+    <slot />
+  </main>
+  <aside class="sidebar col">sidebar</aside>
+</div>
