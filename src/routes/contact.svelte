9644200d5a65228819d1be5846dcccf802f01823
Milo Zheng <hello@milo.works>
2020-09-18 16:36:59 +1000

    feat: apply grid columns to projects and contact pages

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,17 +1,34 @@
+<style>
+  .heading,
+  .body {
+    --width-sm: 12;
+    --push-sm: 1;
+  }
+</style>
+
 <svelte:head>
   <title>Contact</title>
 </svelte:head>
 
-<h1>Say Hi</h1>
+<div class="row">
+  <div class="col heading">
+    <h1>Say Hi</h1>
+  </div>
+</div>
 
-<p>
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sollicitudin
-  ullamcorper leo et convallis. Mauris nec pretium nunc.
-</p>
-<p>
-  Nulla quis nisl quis diam dapibus dictum. Etiam euismod sagittis enim, eu
-  vehicula justo. Vestibulum aliquet et augue at tristique. Integer id neque sit
-  amet diam rhoncus ullamcorper. Praesent vestibulum maximus quam, sit amet
-  gravida dolor euismod at. Aliquam pharetra porta tortor, in sodales ante
-  hendrerit pharetra.
-</p>
+<div class="row">
+  <div class="col body">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+      sollicitudin ullamcorper leo et convallis. Mauris nec pretium nunc.
+    </p>
+    <p>
+      Nulla quis nisl quis diam dapibus dictum. Etiam euismod sagittis enim, eu
+      vehicula justo. Vestibulum aliquet et augue at tristique. Integer id neque
+      sit amet diam rhoncus ullamcorper. Praesent vestibulum maximus quam, sit
+      amet gravida dolor euismod at. Aliquam pharetra porta tortor, in sodales
+      ante hendrerit pharetra.
+    </p>
+
+  </div>
+</div>
